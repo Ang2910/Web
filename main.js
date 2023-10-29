57111506030c418b5918ceaaf4f62b7adaf935d0
Ang2910 <91636896+Ang2910@users.noreply.github.com>
2023-10-28 19:00:28 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('.birthdayCard').addEventListener('click', function() {
+    const cardImage = document.querySelector('.imageContainer');
+    cardImage.style.display = 'block';
+  });
+  
+  
